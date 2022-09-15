5b181e32ef9b3d6464ff2bea356d9eb90e6456ac
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2022-09-15 10:26:46 +0530

    Update mlflowruns.py

--- mlflowruns.py
+++ mlflowruns.py
@@ -7,7 +7,6 @@ import sys
 import pandas as pd
 import numpy as np
 import pickle
-import streamlit as st
 import pickle
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
