16b4f7d038bd154a65b259399a49ce6b3051eaab
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2023-06-10 09:38:13 +0530

    Add files via upload

--- data_analysis.py
+++ data_analysis.py
@@ -1,28 +1,15 @@
-from load_data import load_data
-'''import numpy as np
-import pandas as pd'''
-'''import seaborn as sns'''
-'''import matplotlib.pyplot as plt'''
+from loaddata import Load_Data
 def data_analysis():
-    data = load_data()
-    #data = pd.read_csv('bank.csv')
-    features_na = [features for features in data.columns if data[features].isnull().sum() > 0]
-    for feature in features_na:
-        print(feature, np.round(data[feature].isnull().mean()))
-    else:
-        print("no missing value found")
-    # Find Features with One Value
-    for column in data.columns:
-        print(column,data[column].nunique())
-    #Exploring the Categorical Features
-    categorical_features = [feature for feature in data.columns if ((data[feature].dtypes=='O') & (feature not in ['y']))]
-    print(categorical_features)
-    for feature in categorical_features:
-        print('The feature is {} and number of categories are {}'.format(feature,len(data[feature].unique())))
-    #list of numerical features
-    numerical_features = [feature for feature in data.columns if ((data[feature].dtypes != 'O') & (feature not in ['y']))]
-    print('Number of numerical variables:', len(numerical_features))
-    return data
-    
-
+    dataset = Load_Data()
+   # Providing the details of data 
+    print(dataset.columns)
+    print(dataset.head())
+    print(dataset.shape)
+    print(dataset.dtypes)
+    print(dataset.info())
+    print(dataset.describe)
+    print(dataset.isnull().sum())
+    for col in dataset.columns:
+        print(col, len(dataset[col].unique()))
+    return dataset
 data_analysis()
